90974dc244077610135da2b8f95011077b2fdd8e
Bista Solutions <sales@bistasolutions.com>
2015-09-16 15:54:25 +0530

    [ADD] add module to freeze the status-bar header.

---
+++ bista_freeze_header/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Freeze Header',
+    'version': '1.1',
+    'description': """Freeze Header""",
+    'author': 'Bista Solutions Pvt. Ltd',
+    'website': 'http://www.bistasolutions.com',
+    'depends': [],
+    'category': 'web',
+    'sequence': 16,
+    'data': ['views/freeze_header.xml'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
